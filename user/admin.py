dc620d8959991612779a32644c800f717e85020b
aditi-pandey00 <174866754+aditi-pandey00@users.noreply.github.com>
2024-08-12 13:37:45 +0530

    Add files via upload

---
+++ user/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+class contactAdmin(admin.ModelAdmin):
+    list_display=("name","mobile","email","message")
+admin.site.register(contact,contactAdmin)
+
+
+class citiesAdmin(admin.ModelAdmin):
+    list_display=("id","cname","cpic","cdate")
+admin.site.register(cities,citiesAdmin)
+
+
+class addplaceAdmin(admin.ModelAdmin):
+    list_display=( "id","ptitle","city","baplace","pdes","placepic","pdate")
+admin.site.register(addplace,addplaceAdmin)
+
+
+
+class guiderAdmin(admin.ModelAdmin):
+    list_display=("name","city","guiderpic","qualification","gid","gidp","address","gdate","mobile")
+admin.site.register(guider,guiderAdmin)
+
+
+class galleryAdmin(admin.ModelAdmin):
+    list_display=("pdes","picture","gdate")
+admin.site.register(gallery,galleryAdmin)
+
